6194ce8864b2293ef2a8e6cd90cd9b3762dd4428
barnabas <barnabas.lewis@ticketmaster.com>
2021-12-16 16:57:57 -0600

    wip

--- newworkspaceshortcut@barnix.io/prefs.js
+++ newworkspaceshortcut@barnix.io/prefs.js
@@ -28,13 +28,15 @@ function buildPrefsWidget() {
     });
     prefsWidget.set_center_widget(gridWidget);
 
-    let titlespace = new Gtk.Label({
+
+    let title = new Gtk.Label({
         label: ` `,
         halign: Gtk.Align.CENTER,
         use_markup: true,
         visible: true
     });
-    gridWidget.attach(titlespace, 0, 0, 2, 1);
+    gridWidget.attach(title, 0, 0, 2, 1);
+
 
     let project = new Gtk.Label({
         label: `<a href="https://github.com/barnscott/newworkspaceshortcut-barnix.io#tldr">How to use this extension</a>`,
@@ -43,7 +45,6 @@ function buildPrefsWidget() {
         visible: true
     });
     gridWidget.attach(project, 0, 1, 2, 1);
-
     let report = new Gtk.Label({
         label: `<a href="https://github.com/barnscott/newworkspaceshortcut-barnix.io/issues">Report a bug</a>`,
         halign: Gtk.Align.CENTER,
@@ -52,21 +53,129 @@ function buildPrefsWidget() {
     });
     gridWidget.attach(report, 0, 2, 2, 1);
 
+    
+    let space0 = new Gtk.Label({
+        label: ` `,
+        halign: Gtk.Align.CENTER,
+        use_markup: true,
+        visible: true
+    });
+    gridWidget.attach(space0, 0, 3, 2, 1);
+
+
+    let mwr = new Gtk.Label({
+        label: 'Move-window-to-new-workspace right',
+        halign: Gtk.Align.START,
+        visible: true
+    });
+    gridWidget.attach(mwr, 0, 4, 1, 1);
+    let mwrs = new Gtk.Label({
+        label: 'Super + Alt + Shift + n',
+        halign: Gtk.Align.START,
+        visible: true
+    });
+    gridWidget.attach(mwrs, 1, 4, 1, 1);
+
+    let mwl = new Gtk.Label({
+        label: 'Move-window-to-new-workspace left',
+        halign: Gtk.Align.START,
+        visible: true
+    });
+    gridWidget.attach(mwl, 0, 5, 1, 1);
+    let mwls = new Gtk.Label({
+        label: 'Super + Alt + Shift + b',
+        halign: Gtk.Align.START,
+        visible: true
+    });
+    gridWidget.attach(mwls, 1, 5, 1, 1);
+
+
+    let space1 = new Gtk.Label({
+        label: ` `,
+        halign: Gtk.Align.CENTER,
+        use_markup: true,
+        visible: true
+    });
+    gridWidget.attach(space1, 0, 6, 2, 1);
+
+
+    let ewr = new Gtk.Label({
+        label: 'New-empty-workspace right',
+        halign: Gtk.Align.START,
+        visible: true
+    });
+    gridWidget.attach(ewr, 0, 7, 1, 1);
+    let ewrs = new Gtk.Label({
+        label: 'Super + Alt + n',
+        halign: Gtk.Align.START,
+        visible: true
+    });
+    gridWidget.attach(ewrs, 1, 7, 1, 1);
+
+
+    let ewl = new Gtk.Label({
+        label: 'New-empty-workspace left',
+        halign: Gtk.Align.START,
+        visible: true
+    });
+    gridWidget.attach(ewl, 0, 8, 1, 1);
+    let ewls = new Gtk.Label({
+        label: 'Super + Alt + b',
+        halign: Gtk.Align.START,
+        visible: true
+    });
+    gridWidget.attach(ewls, 1, 8, 1, 1);
+
+
+    let space2 = new Gtk.Label({
+        label: ` `,
+        halign: Gtk.Align.CENTER,
+        use_markup: true,
+        visible: true
+    });
+    gridWidget.attach(space2, 0, 9, 2, 1);
+
+
+    let rwsr = new Gtk.Label({
+        label: 'Reorder-workspace right',
+        halign: Gtk.Align.START,
+        visible: true
+    });
+    gridWidget.attach(rwsr, 0, 10, 1, 1);
+    let rwsrs = new Gtk.Label({
+        label: 'Ctl + Super + Right',
+        halign: Gtk.Align.START,
+        visible: true
+    });
+    gridWidget.attach(rwsrs, 1, 10, 1, 1);
+
+
+    let rwsl = new Gtk.Label({
+        label: 'Reorder-workspace left',
+        halign: Gtk.Align.START,
+        visible: true
+    });
+    gridWidget.attach(rwsl, 0, 11, 1, 1);
+    let rwsls = new Gtk.Label({
+        label: 'Ctl + Super + Left',
+        halign: Gtk.Align.START,
+        visible: true
+    });
+    gridWidget.attach(rwsls, 1, 11, 1, 1);
+
+
     // Add toggle to override default
     let toggleLabel = new Gtk.Label({
         label: 'Reorder-workspace shortcut will trigger Overview:',
         halign: Gtk.Align.START,
         visible: true
     });
-    gridWidget.attach(toggleLabel, 0, 3, 1, 1);
-
+    gridWidget.attach(toggleLabel, 0, 12, 1, 1);
     let toggle = new Gtk.Switch({
         active: this.settings.get_boolean ('move-ws-triggers-overview'),
-        halign: Gtk.Align.END,
+        halign: Gtk.Align.START,
         visible: true
     });
-    gridWidget.attach(toggle, 1, 3, 1, 1);
-
     // Bind toggle to `moveWSTriggersOverview`
     this.settings.bind(
         'move-ws-triggers-overview',
@@ -74,6 +183,17 @@ function buildPrefsWidget() {
         'active',
         Gio.SettingsBindFlags.DEFAULT
     );
+    gridWidget.attach(toggle, 1, 12, 1, 1);
+
+
+
+    let space3 = new Gtk.Label({
+        label: ` `,
+        halign: Gtk.Align.CENTER,
+        use_markup: true,
+        visible: true
+    });
+    gridWidget.attach(space3, 0, 13, 2, 1);
 
     return prefsWidget;
 }
